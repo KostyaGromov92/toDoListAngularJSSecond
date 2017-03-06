842d1f0420593bd978401a780c327abbc8d19b65
DESKTOP-52VJMD4\Костя <kotya@zeoalliance.com>
2017-03-06 18:01:35 +0200

    Update logic

--- script/controller.js
+++ script/controller.js
@@ -1,22 +1,27 @@
 var app = angular.module('myApp',[]);
 
-app.controller('ListController', function ($scope) { 
+app.controller('ListController', function ($scope) {
 $scope.todos = [
-    {text:'Learn AngularJS', done:false},         
+    {text:'Learn AngularJS', done:false},
     {text: 'Build an app', done:false}
   ];
-  
+
   $scope.getTotalTodos = function () {
     return $scope.todos.length;
   };
-  
-  
+
+
   $scope.addTodo = function () {
-      $scope.todos.push({text:$scope.formTodoText, done:false});
-      $scope.formTodoText = '';
-  
+
+      if($scope.formTodoText == undefined) {
+        return;
+      } else {
+        $scope.todos.push({text:$scope.formTodoText, done:false});
+        $scope.formTodoText = '';
+      }
+
   };
-  
+
     $scope.clearCompleted = function () {
         angular.forEach($scope.todos, function (todo) {
           return todo.done = false;
